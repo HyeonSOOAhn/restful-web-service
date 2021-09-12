39452381d7e31bfa8e55f27e1bfaa48b6e84ddca
hyeonsooahn <djthqtu1@naver.com>
2021-09-12 21:47:11 +0900

    commit

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,11 @@
-package com.example.restfulwebservice;
+package com.example.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloCWorldController {
+public class HelloWorldController {
 
     //GET
     // /hello-world -> endpoint
